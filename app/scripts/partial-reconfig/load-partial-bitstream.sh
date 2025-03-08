49a0af9d759dcb78813925ecb734b62c9a7a20eb
Rigo Orozco <rigo.orozco.d@gmail.com>
2025-03-08 20:21:02 +0100

    Add partial reconfig scripts

---
+++ app/scripts/partial-reconfig/load-partial-bitstream.sh
@@ -0,0 +1,34 @@
+#! /bin/sh
+
+# Check the number of arguments
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <bitstream>"
+    exit 1
+fi
+
+SCRIPT_DIR=$(dirname "$0")
+PARTIAL_BITSTREAM=$1
+
+# Check if bitstream exits
+if [ ! -f ${PARTIAL_BITSTREAM} ]; then
+    echo "Bitstream ${PARTIAL_BITSTREAM} not found!"
+    exit 1
+fi
+
+# Addresses
+DFX_DECOUPLER_DISCONNECT_REG=0x2000
+DFX_DECOUPLER_STATUS_REG=0x2008
+HWICAP_BASE_ADDR=0x1000
+
+# Disconnect partition
+OUTPUT=$(sh -c "${SCRIPT_DIR}/../../xdma-tools/reg_rw /dev/xdma0_user ${DFX_DECOUPLER_DISCONNECT_REG} w 1")
+
+# Load partial bistream
+${SCRIPT_DIR}/../../hwicap_write_bitstream/hwicap_write_bitstream /dev/xdma0_user ${HWICAP_BASE_ADDR} ${PARTIAL_BITSTREAM}
+
+# Re-connect partition
+OUTPUT=$(sh -c "${SCRIPT_DIR}/../../xdma-tools/reg_rw /dev/xdma0_user ${DFX_DECOUPLER_DISCONNECT_REG} w 0")
+
+# Read status
+OUTPUT=$(sh -c "${SCRIPT_DIR}/../../xdma-tools/reg_rw /dev/xdma0_user ${DFX_DECOUPLER_STATUS_REG} w | tail -c 11")
+echo "Decoupler status is ${OUTPUT} after loading bitstream"
---
+++ app/scripts/partial-reconfig/test-loading.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+SCRIPT_DIR=$(dirname "$0")
+
+DFX_GPIO_CH2_REG=0x12008
+
+# Write something
+OUTPUT=$(sh -c "${SCRIPT_DIR}/../../xdma-tools/reg_rw /dev/xdma0_user ${DFX_GPIO_CH2_REG} w 0x0badf00d")
+
+# Read value back
+OUTPUT=$(sh -c "${SCRIPT_DIR}/../../xdma-tools/reg_rw /dev/xdma0_user ${DFX_GPIO_CH2_REG} w | tail -c 11")
+echo "Wrote ${OUTPUT} to register"
+
+# Load partial bitstream
+${SCRIPT_DIR}/load-partial-bitstream.sh ${SCRIPT_DIR}/../../../projects/xdma_ddr3_dfx/build/static/xdma_ddr3_dfx_pblock_rm_partial.bin
+
+# Read current value
+OUTPUT=$(sh -c "${SCRIPT_DIR}/../../xdma-tools/reg_rw /dev/xdma0_user ${DFX_GPIO_CH2_REG} w | tail -c 11")
+echo "Register value afer loading partial bitstream: ${OUTPUT}"
