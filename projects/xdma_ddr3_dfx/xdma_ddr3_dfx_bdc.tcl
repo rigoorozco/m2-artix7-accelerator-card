ac7018f74271a007542bb5b129f78e741ed2ba2c
Rigo Orozco <rigo.orozco.d@gmail.com>
2025-03-08 18:47:37 +0100

    Enable second channel in gpio controller inside partion for peeking and poking

--- projects/xdma_ddr3_dfx/xdma_ddr3_dfx_bdc.tcl
+++ projects/xdma_ddr3_dfx/xdma_ddr3_dfx_bdc.tcl
@@ -275,7 +275,11 @@ proc create_root_design { parentCell } {
 
   # Create instance: axi_gpio_0, and set properties
   set axi_gpio_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_gpio:2.0 axi_gpio_0 ]
-  set_property CONFIG.C_ALL_INPUTS {1} $axi_gpio_0
+  set_property -dict [list \
+    CONFIG.C_ALL_INPUTS {1} \
+    CONFIG.C_ALL_OUTPUTS_2 {1} \
+    CONFIG.C_IS_DUAL {1} \
+  ] $axi_gpio_0
 
 
   # Create instance: axi_interconnect_0, and set properties
