d15a349accacecf3138205253e74d7e2b55c2c78
Rigo Orozco <rigo.orozco.d@gmail.com>
2025-03-11 01:13:38 +0100

    End my dma test woes. Use interrupt mode 0

--- app/scripts/kernel-module/load.sh
+++ app/scripts/kernel-module/load.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(dirname "$0")
 
 # install new module into kernel (must have been compiled and installed to extras)
-insmod ${SCRIPT_DIR}/../../xdma-kernel-module/xdma/xdma.ko interrupt_mode=1
+insmod ${SCRIPT_DIR}/../../xdma-kernel-module/xdma/xdma.ko interrupt_mode=0
 
 # rescan PCI bus to install driver for this card
 sh -c "echo 1 > /sys/bus/pci/rescan"
