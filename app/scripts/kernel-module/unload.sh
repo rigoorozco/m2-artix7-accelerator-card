73f3948b836ab48e821a62d9b78acca7f16434e7
Rigo Orozco <rigo.orozco.d@gmail.com>
2025-03-11 20:35:10 +0100

    Use lspci to unload xdma driver

--- app/scripts/kernel-module/unload.sh
+++ app/scripts/kernel-module/unload.sh
@@ -1,5 +1,13 @@
 #! /bin/sh
 
+# Get PCIe ID
+ID=$(lspci -n -d 10ee: | cut -d ' ' -f 1)
+
+# Exit if no ID is found
+if [ -z "${ID}" ]; then
+    exit 1
+fi
+
 # remove xilinx card and unload kernel driver module
-sh -c "echo 1 > /sys/bus/pci/devices/0000:02:00.0/remove"
+sh -c "echo 1 > /sys/bus/pci/devices/0000:${ID}/remove"
 rmmod xdma
